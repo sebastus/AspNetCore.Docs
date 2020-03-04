9acc952abd2fbbbad373bde1c8508ca8b01d4da3
Greg Oliver <golive@microsoft.com>
2020-03-04 13:29:57 +0000

    add script to keep container alive

---
+++ aspnetcore/mvc/models/file-uploads/samples/3.x/SampleApp/Tools-Do-Nothing.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+# #######################
+# Sit around and do nothing
+# so that a tools container 
+# can be run and examined
+# #######################
+
+while : ; do
+  echo "Sleeping. zzzz"
+  sleep 5
+done
+
